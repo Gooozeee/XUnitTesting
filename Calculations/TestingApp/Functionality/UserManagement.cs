a691b511eadbd427ce5f3aced012fe13d83eba32
Michal Guzy <michalguzym@gmail.com>
2022-07-18 15:14:39 +0100

    Created class to be tested

---
+++ Calculations/TestingApp/Functionality/UserManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApp.Functionality
+{
+    public record User(string FirstName, string LastName)
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string Phone { get; set; }
+        public bool VerifiedEmail { get; set; } = false;
+    }
+
+    public class UserManagement
+    {
+        private readonly List<User> _users = new();
+        private int idCounter = 1;
+
+        public IEnumerable<User> AllUsers => _users;
+
+        public void Add(User user)
+        {
+            _users.Add(user with { Id = idCounter++ });
+        }
+
+        public void UpdatePhone(User user)
+        {
+            var dbUser = _users.First(u => u.Id == user.Id);
+
+            dbUser.Phone = user.Phone;
+        }
+
+        public void VerifyEmail(int userId)
+        {
+            var dbUser = _users.First(u => u.Id == userId);
+
+            dbUser.VerifiedEmail = true;
+        }
+    }
+}
